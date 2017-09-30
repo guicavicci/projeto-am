3af9697295837882464be577e99b4e1015505a0d
gneves <gneves@debian-gui>
2017-09-30 19:11:49 -0300

    dev-inicio

--- am/src/beans/PessoaFisica.java
+++ am/src/beans/PessoaFisica.java
@@ -1,56 +1,22 @@
 package beans;
 
 
-public class PessoaFisica extends Pessoa{
+public class PessoaFisica {
 	private String cpf;
 	private String rg;
 	private String dataNascimento;
 	
 	public String getAll () {
-		return super.getAll() + "\n" + cpf + "\n" + rg + "\n" + dataNascimento;
+		return cpf + "\n" + rg + "\n" + dataNascimento;
 		
 	}
 	
-	public void setAll(int i, String n, String em, String t, String c, String r, String dn) {
-		super.setAll(i, n, em, t);
+	public void setAll(String c, String r, String dn) {
 		cpf =c;
 		rg=r;
 		dataNascimento = dn;
 		
 	}
-	
-	
-	public PessoaFisica() {
-		super();
-	}
-	public PessoaFisica(int id, String nome, String email, String telefone, String cpf, String rg,
-			String dataNascimento, Pessoa pessoa) {
-		super(id, nome, email, telefone);
-		this.cpf = cpf;
-		this.rg = rg;
-		this.dataNascimento = dataNascimento;
-
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getRg() {
-		return rg;
-	}
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento= dataNascimento;
-	}
-
-	
 	
 	
 }
